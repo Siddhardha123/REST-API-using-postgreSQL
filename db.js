4eacb468f3d3e71e617b81048412a3a4e0b705a6
siddhardha varma <sid@pop-os.localdomain>
2022-11-29 21:43:05 +0530

    api done

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
-
+require('dotenv').config();
 const Pool = require('pg').Pool;
 const pool = new Pool({
     user : "postgres",
     host : "localhost",
     database: "students",
-    password: "test1234",
+    password: process.env.PASSWORD,
     port : 5432,
 
 });
